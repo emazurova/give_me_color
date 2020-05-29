f26bda13348fa9959ae3c41c56bc05be077c2205
Elizaveta <kelizaveta130598@gmail.com>
2020-05-29 12:27:57 +0300

    Fix method dependencies and add errors handling

--- ColorsGetter.py
+++ ColorsGetter.py
@@ -1,5 +1,4 @@
 from sklearn.cluster import KMeans
-from ImagePreparation import ImagePreparation
 
 
 class ColorsGetter:
@@ -7,10 +6,21 @@ class ColorsGetter:
         self.image = image
         self.height, self.width = self.image.size
         self.data = image.getdata()
-        self.colors = ImagePreparation.normalize(image.getcolors(self.height * self.width), True)
+        self.colors = self.normalize_colors(image.getcolors(self.height * self.width), True)
+
+    @staticmethod
+    def normalize_colors(init_colors, is_tuple):
+        normalized_colors = list()
+        if is_tuple:
+            for item in init_colors:
+                normalized_colors.append(list([x / 255 for x in item[1]]))
+        else:
+            for item in init_colors:
+                normalized_colors.append(list([x / 255 for x in item]))
+        return normalized_colors
 
     def get_kmeans_colors(self, clusters_count):
-        return ImagePreparation.normalize(KMeans(n_clusters=clusters_count).fit(self.data).cluster_centers_, False)
+        return self.normalize_colors(KMeans(n_clusters=clusters_count).fit(self.data).cluster_centers_, False)
 
     def get_average_color(self):
         length = len(self.colors)
@@ -26,15 +36,15 @@ class ColorsGetter:
 
         return [(r_total, g_total, b_total)]
 
-    def get_most_freq_colors(self, colors_count = 256):
+    def get_most_freq_colors(self, colors_count=256):
         if colors_count > 256:
-            yield ValueError("Colors must be in range [1;256]")
+            raise ValueError("Colors must be in range [1;256]")
         quantized = self.image.quantize(colors=colors_count)
         convert_rgb = quantized.convert('RGB')
         colors = convert_rgb.getcolors()
         color_str = sorted(colors, reverse=True)
 
-        final_list = []
+        final_colors_list = []
         for i in color_str:
-            final_list.append(i[1]/255)
-        return final_list
+            final_colors_list.append(i[1])
+        return self.normalize_colors(final_colors_list, False)
--- ImagePreparation.py
+++ ImagePreparation.py
@@ -1,12 +1,10 @@
 from PIL.Image import new, open, ROTATE_90
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 
 
 class ImagePreparation:
     def __init__(self, images_path, needed_size):
         self.images_path = images_path
-        self.images = self.open_images()
+        self.images = list()
         self.needed_size = needed_size
 
     def __del__(self):
@@ -14,6 +12,9 @@ class ImagePreparation:
             item.close()
 
     def open_images(self):
+        if len(self.images_path) == 0:
+            raise ValueError("There are no images to open")
+
         images = list()
         for path in self.images_path:
             images.append(open(path))
@@ -39,16 +40,12 @@ class ImagePreparation:
     def concat_horizontally(images_to_concat):
         w = images_to_concat[0].width * len(images_to_concat)
         h = images_to_concat[0].height
-
         img = new('RGB', (w, h))
 
         for index, item in enumerate(images_to_concat):
             img.paste(item, (item.width * index, 0))
 
-        filepath = "hor{}.{}".format(images_to_concat[0].width,"png")
-        img.save(filepath)
         return img
-
 
     @staticmethod
     def concat_vertically(images_to_concat):
@@ -57,40 +54,12 @@ class ImagePreparation:
         img = new('RGB', (w, h))
         for index, item in enumerate(images_to_concat):
             img.paste(item, (0, item.height * index))
-        img.save("ver.png")
         return img
 
-    @staticmethod
-    def normalize(to_normalize, is_tuple):
-        normalized_colors = list()
-        if is_tuple:
-            for item in to_normalize:
-                normalized_colors.append(list([x / 255 for x in item[1]]))
-        else:
-            for item in to_normalize:
-                normalized_colors.append(list([x / 255 for x in item]))
-        return normalized_colors
-
     def prepare(self):
+        self.images = self.open_images()
         self.make_rotation()
         self.correct_rescale()
         img_1 = self.concat_horizontally(self.images[:3])
         img_2 = self.concat_horizontally(self.images[3:])
         return self.concat_vertically([img_1, img_2])
-
-    @staticmethod
-    def show_image_with_colors(image, colors, to_save=False, filename=None):
-        img_to_show = new('RGB', (image.width, image.height + 80))
-        img_to_show.paste(image, (0, 0))
-
-        figure, ax = plt.subplots(1)
-        plt.axis('off')
-        w = image.width / len(colors)
-        ax.imshow(img_to_show)
-
-        for item in range(len(colors)):
-            wid = w * item
-            ax.add_patch(patches.Rectangle((wid, image.height), w, 80, edgecolor='none', facecolor=colors[item]))
-
-        if to_save:
-            plt.savefig(filename)
